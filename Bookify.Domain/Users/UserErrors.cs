e984c35a1a3fc87601a97e875a6a9c2e4d5ae8b6
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-28 23:10:49 +0545

    add user errors

--- Bookify.Domain/Users/UserErrors.cs
+++ Bookify.Domain/Users/UserErrors.cs
@@ -1,6 +1,15 @@
-﻿namespace Bookify.Domain.Users;
+﻿using Bookify.Domain.Abstractions;
 
-public class UserErrors
+namespace Bookify.Domain.Users;
+
+
+public static class UserErrors
 {
-    
+    public static readonly Error NotFound = new(
+        "User.Found",
+        "The user with the specified identifier was not found");
+
+    public static readonly Error InvalidCredentials = new(
+        "User.InvalidCredentials",
+        "The provided credentials were invalid");
 }
