26222d1884cc3c73e5b8ece9af075a8836e8750d
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-05-28 23:10:24 +0545

    add repository fro booking

--- Bookify.Domain/Bookings/IBookingRepository.cs
+++ Bookify.Domain/Bookings/IBookingRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Bookify.Domain.Bookings;
+﻿
+namespace Bookify.Domain.Bookings;
 
-public class IBookingRepository
+public interface IBookingRepository
 {
-    
+    Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> IsOverlappingAsync(
+        Apartment.Apartment apartment,
+        DateRange duration,
+        CancellationToken cancellationToken = default);
+
+    void Add(Booking booking);
 }
